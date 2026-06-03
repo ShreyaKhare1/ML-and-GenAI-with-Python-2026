a36674f83f7fcf4871bba12107c98673e5e42157
tech123-prog <ishika08dec.06@gmail.com>
2026-06-03 16:00:30 +0530

    Delete Assignment_1/Ishika_Gupta_02501222025/Exercise2.py

--- Assignment_1/Ishika_Gupta_02501222025/Exercise2.py
+++
@@ -1,8 +0,0 @@
-# Aim-Find simple interest
-p = float(input("Enter Principal: "))
-r = float(input("Enter Rate of Interest: "))
-t = float(input("Enter Time(in years): "))
-
-simple_interest = (p * r * t) / 100
-
-print("Simple Interest =", simple_interest)
